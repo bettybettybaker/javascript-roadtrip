281274071b74a6bcc36e1ee3ee87716e19a7c4d7
Betty Baker <betty@heysparkbox.com>
2017-03-14 15:46:39 -0400

    Boolean Expression < ex4 < Level 1 [SOLVED]

---
+++ js-roadtrip-1/lvl-1/04_boolean-expression.js
@@ -0,0 +1,13 @@
+// Challenge:
+
+// Write a Boolean expression that tests whether two numbers
+// are equal, and whose answer evaluates to false.
+
+
+///////////////////////// Solution /////////////////////////
+
+4 == 3
+
+// == is a comparative operater
+// since 4 is not equal to 3,
+// this expression evaluates to false
