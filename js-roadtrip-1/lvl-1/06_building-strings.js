67987d75e743a4952f2bcd53f3d182bd0b63c1fd
Betty Baker <betty@heysparkbox.com>
2017-03-14 15:57:07 -0400

    Building Strings < ex6 < Level 1 [SOLVED]

---
+++ js-roadtrip-1/lvl-1/06_building-strings.js
@@ -0,0 +1,11 @@
+// Challenge:
+
+// Create a String of your first and last name, with the first
+// letter of each word capitalized, and each word separated by a space.
+
+
+
+
+///////////////////////// Solution /////////////////////////
+
+"Betty Baker"
