04cd39ba6277685883e335e656f28dbdb440e68e
Betty Baker <betty@heysparkbox.com>
2017-03-14 16:50:50 -0400

    Variable 3 < ex3 < Level 2 [SOLVED]

---
+++ js-roadtrip-1/lvl-2/03_variable-3.js
@@ -0,0 +1,8 @@
+// Challenge:
+
+// Declare a variable called firstName and store your first name inside it.
+
+
+///////////////////////// SOLUTION /////////////////////////
+
+var firstName = "Betty"
