c88f5935769820e8f8b900e669ca06ea83fb5256
Betty Baker <betty@heysparkbox.com>
2017-03-14 16:52:56 -0400

    Variable 4 < ex4 < Level 2 [SOLVED]

---
+++ js-roadtrip-1/lvl-2/04_variable-4.js
@@ -0,0 +1,8 @@
+// Challenge:
+
+// Now declare a variable called lastName and store your last name inside it.
+
+
+///////////////////////// SOLUTION /////////////////////////
+
+var lastName = "Baker"
