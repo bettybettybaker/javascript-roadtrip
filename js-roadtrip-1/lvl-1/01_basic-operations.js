f6587072c0527540f710a54c8c921409665d350d
Betty Baker <betty@heysparkbox.com>
2017-03-14 14:53:04 -0400

    Add js-roadtrip-1, exercise 1 - basic operations

---
+++ js-roadtrip-1/lvl-1/01_basic-operations.js
@@ -0,0 +1,10 @@
+// Challenge:
+// In the console, enter any expression which
+// uses +, -, *, or /, and whose answer evaluates to 36.
+
+
+///////////////////////// Solution /////////////////////////
+
+30 + 6
+
+// returns 36
