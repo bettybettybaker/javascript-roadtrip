e3af63c08b5b1bd70f86efd2ef1fb0cbdbbc1295
Betty Baker <betty@heysparkbox.com>
2017-03-14 15:49:46 -0400

    Another Boolean Expression < ex5 < Level 1 [SOLVED]

---
+++ js-roadtrip-1/lvl-1/05_another-boolean-expression.js
@@ -0,0 +1,14 @@
+// Challenge:
+
+// Write a Boolean expression that tests whether two numbers
+// are not equal, and whose answer evaluates to true.
+
+
+
+///////////////////////// Solution /////////////////////////
+
+4 != 3
+
+// A bang (!) in this context means 'not'
+// so this expression asserts that 4 is not equal to 3.
+// Since 4 is not equal to 3, this expression evaluates to true
