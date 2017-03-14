c509286685a9b1a5500a353c8e6a7a7f28f3299c
Betty Baker <betty@heysparkbox.com>
2017-03-14 16:06:04 -0400

    Booleans with Strings < ex8 < Level 1 [SOLVED]

---
+++ js-roadtrip-1/lvl-1/08_booleans-with-strings.js
@@ -0,0 +1,8 @@
+// Challenge:
+
+// Build a Boolean expression that compares two Strings and evaluates to true.
+
+
+///////////////////////// Solution /////////////////////////
+
+"Harry Potter is cool." == "Harry Potter is cool."
