9e1a4ade751aa8f08404693c6d03a71959b2dbd9
Betty Baker <betty@heysparkbox.com>
2017-03-14 16:55:17 -0400

    Variable Concatenation < ex5 < Level 2

---
+++ js-roadtrip-1/lvl-2/05_variable-concatenation.js
@@ -0,0 +1,14 @@
+// Challenge:
+
+// Using your firstName and lastName variables,
+// make a variable called fullName that unites your
+// first and last names into one String.
+
+
+
+///////////////////////// SOLUTION /////////////////////////
+
+var firstName = "John"
+undefined
+var lastName = "Doe"
+undefined
