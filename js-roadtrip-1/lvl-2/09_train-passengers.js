f1ef2133eaa734136b6c965a3d7de3253a7d4e2d
Betty Baker <betty@heysparkbox.com>
2017-03-14 17:22:00 -0400

    Train Passengers p2 < ex9 < Level 2

---
+++ js-roadtrip-1/lvl-2/09_train-passengers.js
@@ -0,0 +1,16 @@
+// Challenge:
+
+// When these folks get on board, the new passenger count is reported
+// to the conductor. Create a countMessage variable, and assign a String
+// that uses numPassengers to say:
+
+// Attention: There are now 3 passengers on the train!
+
+
+// Remember, no hard-coding values!
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+var numPassengers = 0
+numPassengers += 3
