f2fac4c75cf0ae53eb92c2512d1de3891bc192a6
Betty Baker <betty@heysparkbox.com>
2017-03-14 17:30:16 -0400

    Sum < ex11 < Level 2 [SOLVED]

---
+++ js-roadtrip-1/lvl-2/11_sum.js
@@ -0,0 +1,8 @@
+// Challenge:
+
+// On one line of code, declare the variable twenty20 and assign to it 3 + 10 + 7
+
+
+///////////////////////// SOLUTION /////////////////////////
+
+var twenty20 = 3 + 10 + 7
