6ff30f7c2a855a3956d243d86284101216734608
Betty Baker <betty@heysparkbox.com>
2017-03-14 15:43:18 -0400

    SOLVED: Modulus Operator < ex3 < Level 1

---
+++ js-roadtrip-1/lvl-1/03_modulus-operator.js
@@ -0,0 +1,15 @@
+// Challenge:
+
+// Using the modulus operator %, enter an expression with exactly
+// two values that results in a remainder of 4.
+
+
+
+
+///////////////////////// Solution /////////////////////////
+
+19 % 5
+
+// 19 / 5 = 3 with a remainder of 4
+// modulus (%) returns the remainder only so
+// this expression (19 % 5) evaluates to 4
