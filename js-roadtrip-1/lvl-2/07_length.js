903c861b7eab249a1f938c122ebd5ac00c68b361
Betty Baker <betty@heysparkbox.com>
2017-03-14 17:04:46 -0400

    Length < ex7 < Level 2

---
+++ js-roadtrip-1/lvl-2/07_length.js
@@ -0,0 +1,12 @@
+// Challenge:
+
+// This sentence has been assigned to a variable.
+// Find its length without manually counting any characters or spaces.
+
+// var sentence = "This variable is already assigned. Find its length without manually counting the characters and spaces."
+
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+var sentence = "This variable is already assigned. Find its length without manually counting the characters and spaces."
