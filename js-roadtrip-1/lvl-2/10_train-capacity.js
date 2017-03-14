f3fd00f09329d77a30727a283703351d4e0477e0
Betty Baker <betty@heysparkbox.com>
2017-03-14 17:25:12 -0400

    Train Capacity < ex10 < Level 2

---
+++ js-roadtrip-1/lvl-2/10_train-capacity.js
@@ -0,0 +1,16 @@
+// Challenge:
+
+// 1. Build a trainCapacity variable and set it to 60.
+// 2. Construct a String called occupancy in the following format:
+
+// 3 passengers present, with 57 seats remaining.
+
+// In creating this String, use numPassengers to find the number
+// of passengers present, and (trainCapacity - numPassengers) to
+// find the number of seats remaining.
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+var numPassengers = 0
+numPassengers += 3
