49075e65f126cb6238d6bddb90b4bf0fd9b0566a
Betty Baker <betty@heysparkbox.com>
2017-03-14 16:03:34 -0400

    Special Characters in Strings < ex7 < Level 1 [SOLVED]

---
+++ js-roadtrip-1/lvl-1/07_special-characters-in-strings.js
@@ -0,0 +1,15 @@
+// Challenge:
+
+// Build the following output in one String, using special characters
+// for the newline, tab stop, and quotation marks:
+
+// Quoth the raven:
+//    "Nevermore!"
+
+///////////////////////// Solution /////////////////////////
+
+"Quoth the raven:\n\t\"Nevermore!\""
+
+//          \n = new line
+//          \t = tab
+//          \" = escapes the quote - allows for double quotes inside string
