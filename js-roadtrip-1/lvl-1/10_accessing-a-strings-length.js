b560a04433a874204f8080fbc1bb52fd8c3df04b
Betty Baker <betty@heysparkbox.com>
2017-03-14 16:27:01 -0400

    Accessing a String's Length < ex10 < Level 1 [SOLVED]

---
+++ js-roadtrip-1/lvl-1/10_accessing-a-strings-length.js
@@ -0,0 +1,11 @@
+// Challenge:
+
+// Create a String with exactly 33 characters.
+// Then, access the length of your String with the .length property.
+
+
+///////////////////////// Solution /////////////////////////
+
+"Solemnly swear I am up to no good".length
+
+// Evaluates to 33
