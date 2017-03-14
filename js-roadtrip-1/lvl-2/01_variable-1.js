ed083efea25cfec4e803d8b35dd3cd866ad23924
Betty Baker <betty@heysparkbox.com>
2017-03-14 16:38:51 -0400

    Variable 1 < ex1 < Level 2 [SOLVED]

---
+++ js-roadtrip-1/lvl-2/01_variable-1.js
@@ -0,0 +1,14 @@
+// Challenge:
+
+// Declare a variable whose name is age and assign your
+// own age to it using a numerical value.
+
+
+///////////////////////// Solution /////////////////////////
+
+var age = 28
+
+// 'var' tells the console that I am about to assign vaule to a variable
+// 'age' is the name of the variable
+// '=' is an assignment operator. It associates the name of the variable with its value
+// '28' is the value of the newly created variable 'age'
