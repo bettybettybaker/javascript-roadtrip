9883801152f6bb0af8ec2662d133409ebf99be92
Betty Baker <betty@heysparkbox.com>
2017-03-14 16:41:43 -0400

    Variable 2 < ex2 < Level 2

---
+++ js-roadtrip-1/lvl-2/02_variable-2.js
@@ -0,0 +1,9 @@
+// Challenge:
+
+// On line 2 (LINE 10 IN THIS EXAMPLE), declare a second variable called ageIn5Years
+// and assign the following expression to it: age + 5
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+var age = 10
