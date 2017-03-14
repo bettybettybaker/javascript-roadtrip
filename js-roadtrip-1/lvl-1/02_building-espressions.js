00e45e02f5bef7c224399f5ea9f2a5dda3a38b5e
Betty Baker <betty@heysparkbox.com>
2017-03-14 15:06:52 -0400

    :racehorse: feat: Add jsrt-1 lvl-1 ex2 building espressions

---
+++ js-roadtrip-1/lvl-1/02_building-espressions.js
@@ -0,0 +1,19 @@
+// Challenge:
+
+// Enter one expression that satisfies all of the following criteria:
+
+// - The answer evaluates to 14.
+// - The expression contains at least 3 different values.
+// - The expression contains exactly 1 set of parentheses.
+
+
+
+///////////////////////// Solution /////////////////////////
+
+17 - (58 % 11)
+
+// Follows PEMDAS order of operations.
+// modulus (%) gives the remainder and evaluates in the same order
+// as multiplication in PEMDAS
+// This evaluates as 58 % 11 = 3 (remainder of 58 / 11)
+// then 17 - 3 = 14
