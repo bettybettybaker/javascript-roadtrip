f23190ca3cbec3370d7db97c72e060b7b0efc8d5
Betty Baker <betty@heysparkbox.com>
2017-03-14 17:05:59 -0400

    Length < ex7 < Level 2 [SOLVED]

--- js-roadtrip-1/lvl-2/07_length.js
+++ js-roadtrip-1/lvl-2/07_length.js
@@ -7,6 +7,10 @@
 
 
 
-///////////////////////// PROBLEM /////////////////////////
+///////////////////////// SOLUTION /////////////////////////
 
 var sentence = "This variable is already assigned. Find its length without manually counting the characters and spaces."
+
+sentence.length
+
+// evaluates to 114
