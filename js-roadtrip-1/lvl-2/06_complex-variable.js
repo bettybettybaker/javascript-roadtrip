f09fd1945da2422f5e201a8711efc8534be08746
Betty Baker <betty@heysparkbox.com>
2017-03-14 17:02:07 -0400

    Complex Variable < ex6 < Level 2 [SOLVED]

---
+++ js-roadtrip-1/lvl-2/06_complex-variable.js
@@ -0,0 +1,17 @@
+// Challenge:
+
+// Use one line of code to declare a variable called index14
+// and assign to it the character at index 14 in this string:
+
+// "What can we do? Keep going!"
+
+// Try to use the charAt method on the string with something like this:
+// "example string".charAt(index). Or you can assign the correct character
+// to the variable by counting manually.
+
+
+///////////////////////// SOLUTION /////////////////////////
+
+var index14 = "What can we do? Keep going!".charAt(14)
+
+// .charAt(14) returns the character that sits at index 14 of the string
