3cf7c7a6d76900c57751b6944c011aac988388cb
Betty Baker <betty@heysparkbox.com>
2017-03-15 10:46:34 -0400

    A Basic While Loop < ex1 < Level 1

---
+++ js-roadtrip-2/lvl-1/01_basic-while-loop.js
@@ -0,0 +1,10 @@
+// Challenge:
+
+// In countdown.js, write a basic while loop that prints to the
+// console all the numbers from 10 to 1 in descending order.
+// We’ve given you a starting variable to get the wheels turning.
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+var num = 10;
