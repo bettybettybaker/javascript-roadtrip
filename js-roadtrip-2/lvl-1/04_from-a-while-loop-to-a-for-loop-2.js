48b856bd5cffdfa6eea73fe68402aca8b56f6805
Betty Baker <betty@heysparkbox.com>
2017-03-15 14:17:30 -0400

    From a While Loop to a For Loop 2 > ex4 > Level 1

---
+++ js-roadtrip-2/lvl-1/04_from-a-while-loop-to-a-for-loop-2.js
@@ -0,0 +1,24 @@
+// Challenge:
+
+// Morph your previous while loop into a for loop that uses the same variable
+// names. Remember, you’ll still need to declare the starting number of sheep
+// and the number of months to print outside the loop. We’ve given you the
+// starting number of sheep again, as well as the amount of months you’ll
+// need to print out for use in the loop parameters. Here’s a solution for
+// the previous while loop for reference:
+
+// var numSheep = 4;
+// var monthNumber = 1;
+// var monthsToPrint = 12;
+
+// while (monthNumber <= monthsToPrint) {
+//  numSheep = numSheep * 4;
+//  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+//  monthNumber++;
+// }
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+var numSheep = 4;
+var monthsToPrint = 12;
