47e70a3ef91d56360de639d642df11474374fe48
Betty Baker <betty@heysparkbox.com>
2017-03-15 09:32:23 -0400

    Script Tag in HTML < ex1 < Level 3

---
+++ js-roadtrip-1/lvl-3/01_script-tag-in-html.js
@@ -0,0 +1,17 @@
+// Challenge:
+
+// In the index.html file, add a <script> tag inside the <head> section to tell
+// our HTML file that we want to execute some JavaScript code.
+
+// Call your JavaScript file oldFaithful.js. Assume that your .js file is in
+// the same folder as your index.html file.
+
+///////////////////////// PROBLEM /////////////////////////
+
+<html>
+  <head>
+  </head>
+  <body>
+    <h1>Welcome to the JavaScript Express!</h1>
+  </body>
+</html>
