fc3d25725cb2c16d2294551332aac9dc4fdd1a18
Betty Baker <betty@heysparkbox.com>
2017-03-15 09:38:50 -0400

    Using File Paths to Reach a Source File < ex2 < Level 3

---
+++ js-roadtrip-1/lvl-3/02_using-file-paths-to-reach-a-source-file.js
@@ -0,0 +1,26 @@
+// Challenge:
+
+// In the index.html file, add a <script> tag inside the <head> section to tell
+// our HTML file that we want to execute some JavaScript code.
+
+// Call your JavaScript file oldFaithful.js. Here, the files are organized
+// according to a file system structure, and your path is relative to the
+// location of index.html.
+
+//  root/
+//    sitepages/
+//      index.html
+//    scripts/
+//      yellowstone/
+//        oldFaithful.js
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+<html>
+  <head>
+  </head>
+  <body>
+    <h1>Welcome to the JavaScript Express!</h1>
+  </body>
+</html>
