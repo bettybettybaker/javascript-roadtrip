93567121316964df6c9f4f07ee0e228a86cbd2bd
Betty Baker <betty@heysparkbox.com>
2017-03-15 10:10:19 -0400

    Separating Printout Statements < ex5 < Level 3

---
+++ js-roadtrip-1/lvl-3/05_separating-printout-statements.js
@@ -0,0 +1,17 @@
+// Challenge:
+
+// Now, let’s say that Old Faithful’s eruptions are kept as separate variables,
+// as you can see in the oldFaithful.js file.
+
+// We’ve given you the avg variable for the average. Use the existing variables
+// for eruption1, eruption2, eruption3, and numEruptions to calculate the
+// average and assign it to the avg variable.
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+var eruption1 = 112;
+var eruption2 = 148;
+var eruption3 = 160;
+var numEruptions = 3;
+var avg
