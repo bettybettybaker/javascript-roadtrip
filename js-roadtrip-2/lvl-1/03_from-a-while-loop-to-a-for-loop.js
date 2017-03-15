c7663f0377e00a68c66cb5dbe39e63250689c591
Betty Baker <betty@heysparkbox.com>
2017-03-15 14:14:58 -0400

    From a While Loop to a For Loop > ex3 > Level 1 [SOLVED]

---
+++ js-roadtrip-2/lvl-1/03_from-a-while-loop-to-a-for-loop.js
@@ -0,0 +1,20 @@
+// Challenge:
+
+// In countdown.js, write a basic for loop that prints to the console all the
+// numbers from 10 to 1 in descending order. This is the similar to one of the
+// previous challenges, but this time we’re using a for loop instead
+// of a while loop.
+
+// Here is what our while loop looked like from the previous challenge:
+
+//  var num = 10;
+//  while (num > 0) {
+//    console.log(num);
+//    num--;
+//  }
+
+///////////////////////// SOLUTION /////////////////////////
+
+for (var num = 10; num >= 1; num--) {
+	console.log(num);
+}
