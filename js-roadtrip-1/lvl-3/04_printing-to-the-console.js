4c071ae91eb6626e58202850c796c44d442e53b8
Betty Baker <betty@heysparkbox.com>
2017-03-15 10:05:38 -0400

    Printing to the Console < ex4 < Level 3

---
+++ js-roadtrip-1/lvl-3/04_printing-to-the-console.js
@@ -0,0 +1,12 @@
+// Challenge:
+
+// In the oldFaithful.js file, write a statement that prints the following
+// string to the browser console using string concatenation and the existing
+// variable to produce the correct format:
+
+// Average eruption height today: 145
+
+///////////////////////// PROBLEM /////////////////////////
+
+var avgHeight = 145;
+var numEruptions = 4;
