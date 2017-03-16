7ac20cae61828fdf9d8b6c77da4def4a3021d1d0
Betty Baker <betty@heysparkbox.com>
2017-03-16 10:05:00 -0400

    Basic Conditionals < ex01 < Level 2

---
+++ js-roadtrip-2/lvl-2/01_basic-conditional.js
@@ -0,0 +1,18 @@
+// Challenge:
+
+// In countdown.js, modify the while loop with a conditional that
+// will only allow a number to be printed if it is even. Your results
+// should be the even numbers from 10 to 2 in descending order. Think
+// carefully about how your code might decide if a number is even…
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+var num = 10;
+
+while (num > 0) {
+
+    console.log(num);
+
+  num--;
+}
