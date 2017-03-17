46178bef718913ae3afc1877a043b4aa144b7902
Betty Baker <betty@heysparkbox.com>
2017-03-17 09:59:12 -0400

    Problem Solving with Conditionals < ex3 < Level 2 [SOLVED]

--- js-roadtrip-2/lvl-2/03_problem-solving-with-conditionals.js
+++ js-roadtrip-2/lvl-2/03_problem-solving-with-conditionals.js
@@ -21,14 +21,17 @@
   // the provided code.
 
 
-///////////////////////// PROBLEM /////////////////////////
+///////////////////////// SOLUTION /////////////////////////
 
 var numSheep = 4;
 var monthsToPrint = 12;
 
 for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
 
-  <insert your code here>
+  if (numSheep >= 10000) {
+  	numSheep = numSheep / 2
+    console.log("Removing " + numSheep + " sheep from the population.");
+  }
 
   numSheep *= 4;
   console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
