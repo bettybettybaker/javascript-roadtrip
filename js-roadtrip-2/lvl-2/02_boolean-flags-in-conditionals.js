b897eaa655956cb8f82e7d821587633fb6211c5c
Betty Baker <betty@heysparkbox.com>
2017-03-17 09:34:16 -0400

    Boolean Flags in Conditionals < ex2 < Level 2 [SOLVED]

--- js-roadtrip-2/lvl-2/02_boolean-flags-in-conditionals.js
+++ js-roadtrip-2/lvl-2/02_boolean-flags-in-conditionals.js
@@ -15,6 +15,12 @@
 // OR
         // Sorry, the Badlands are particularly bad today. We're closed!
 
-///////////////////////// PROBLEM /////////////////////////
+///////////////////////// SOLUTION /////////////////////////
 
 var parkIsOpen = true;
+
+if (parkIsOpen == true) {
+	console.log("Welcome to the Badlands National Park! Try to enjoy your stay.");
+} else {
+	console.log("Sorry, the Badlands are particularly bad today. We're closed!");
+}
