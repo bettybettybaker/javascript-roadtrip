f8386c86a9530939bbb81d5aff49846328293e34
Betty Baker <betty@heysparkbox.com>
2017-03-20 11:24:07 -0400

    Pop-Up Windows 3 < ex3 < Level 3

---
+++ js-roadtrip-2/lvl-3/03_pop-up-windows-3.js
@@ -0,0 +1,18 @@
+// Challenge:
+
+// With the userAge variable, we can ensure that the user has entered their
+// age correctly. Use a pop-up window function to confirm the user entry
+// with the following message:
+
+      // "You entered <age>. Is this correct?"
+
+// Then store the result of the confirmation in a variable called
+// ageIsCorrect. When you submit your code, enter your age in the prompt
+// window, and then use the confirmation to select whether the entry is
+// correct or incorrect.
+
+// Note: You do not need any loops or conditionals to complete this challenge.
+
+///////////////////////// PROBLEM /////////////////////////
+
+var userAge = prompt("What's your age, user?");
