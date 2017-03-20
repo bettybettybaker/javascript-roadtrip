82cb267be79e5ac9c9d240921fbabd5e0fef8d6c
Betty Baker <betty@heysparkbox.com>
2017-03-20 15:01:34 -0400

    2D Arrays 1 < ex5 < Level 5

---
+++ js-roadtrip-2/lvl-5/05_2D-arrays-1.js
@@ -0,0 +1,19 @@
+// Challenge:
+
+// Check out the following setup. Enter a line of code that
+// declares a variable called infant and accesses the word
+// "Baby" from eightiesMovies without making any changes to
+// either element inside the array.
+
+        // var movie1 = [16, "Candles"];
+        // var movie2 = [3, "Men", "and", "a", "Baby"];
+        // var eightiesMovies = [movie1, movie2];
+
+///////////////////////// PROBLEM /////////////////////////
+
+> var movie1 = [16, "Candles"];
+> var movie2 = [3, "Men", "and", "a", "Baby"];
+> var eightiesMovies = [movie1, movie2];
+undefined
+
+>
