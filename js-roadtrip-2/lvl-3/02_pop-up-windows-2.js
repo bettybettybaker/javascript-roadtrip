cdd8c63959ee62f7fecdeaeddbab165cfc808e66
Betty Baker <betty@heysparkbox.com>
2017-03-20 11:06:06 -0400

    Pop-Up Windows 2 < ex2 < Level 3

---
+++ js-roadtrip-2/lvl-3/02_pop-up-windows-2.js
@@ -0,0 +1,12 @@
+// Challenge:
+
+// In userProfile.js below, ask the user for their age with a pop-up window
+// function, and store the user’s response in a variable called userAge.
+// Use the following question in the dialog box:
+
+// "What's your age, user?"
+
+// When the correct dialog box pops up, enter your age to
+// complete the challenge.
+
+///////////////////////// PROBLEM /////////////////////////
