88cde445e1dbcc50e1f5e807582c3c53f795eab5
Betty Baker <betty@heysparkbox.com>
2017-03-20 14:46:42 -0400

    Array Functions 1 < ex2 < Level 5

---
+++ js-roadtrip-2/lvl-5/02_array-functions-1.js
@@ -0,0 +1,14 @@
+// Challenge:
+
+// Using the specific array function that adds data to the
+// back end of the array, add the next number to your newly
+// corrected list.
+
+// var list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+///////////////////////// PROBLEM /////////////////////////
+
+> var list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+undefined
+
+>
