5b78fe9b01d9af16a27e1b5c68c18a37b6aa8ff6
Betty Baker <betty@heysparkbox.com>
2017-03-20 13:30:15 -0400

    Problem Solving with Functions < ex5 < Level 4

---
+++ js-roadtrip-2/lvl-4/05_problem-solving-with-functions.js
@@ -0,0 +1,24 @@
+// Challenge:
+
+// The Park Rangers in Death Valley National Park divide up the feed
+// responsibilities daily for the Bighorn Sheep population.
+// Each sheep needs 2 lbs of ranger-provided food per day.
+
+// Build a function called feedPerRanger that takes in:
+
+    // The current population of sheep.
+    // The number of Park Rangers available during the day.
+
+// The function should alert the amount of feed that each Park Ranger
+// should be responsible for on that day. The output should match the
+// following format:
+
+// Each Park Ranger should load <number> lbs of feed today.
+
+
+///////////////////////// SOLUTION /////////////////////////
+
+function feedPerRanger(    ) {
+
+
+}
