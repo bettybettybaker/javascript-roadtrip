9aa057447d3664a507eb508680033bcb9440ecec
Betty Baker <betty@heysparkbox.com>
2017-03-20 11:04:18 -0400

    Pop-Up Windows 1 < ex1 < Level 3 [SOLVED]

---
+++ js-roadtrip-2/lvl-3/01_pop-up-windows-1.js
@@ -0,0 +1,16 @@
+// Challenge:
+
+// The Park Rangers at Badlands National Park have decided that the browser
+// console is not a very pretty or efficient way to display their greeting
+// to the user. Change the code below to use a pop-up window that displays
+// to the user whether or not the park is open.
+
+///////////////////////// SOLUTION /////////////////////////
+
+var parkIsOpen = false;
+
+if (parkIsOpen) {
+  alert("Welcome to the Badlands National Park! Try to enjoy your stay.");
+} else {
+  alert("Sorry, the Badlands are particularly bad today. We're closed!");
+}
