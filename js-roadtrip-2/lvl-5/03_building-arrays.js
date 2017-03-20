0b9fb7668f25030a586532877bb2186a24774b2c
Betty Baker <betty@heysparkbox.com>
2017-03-20 14:50:26 -0400

    Building Arrays < ex3 < Level 5

---
+++ js-roadtrip-2/lvl-5/03_building-arrays.js
@@ -0,0 +1,12 @@
+// Challenge:
+
+// Create an array called values that contains the following
+// contents in order:
+
+//      1. a String
+//      2. a Number
+//      3. a Boolean
+
+///////////////////////// PROBLEM /////////////////////////
+
+>
