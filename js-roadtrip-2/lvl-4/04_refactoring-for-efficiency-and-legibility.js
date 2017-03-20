12d30c1a81681c7b794ca388bf942ff14a6e0d1a
Betty Baker <betty@heysparkbox.com>
2017-03-20 13:15:42 -0400

    Refactoring for Efficiency and Legibility < ex4 < Level 4 [SOLVED]

--- js-roadtrip-2/lvl-4/04_refactoring-for-efficiency-and-legibility.js
+++ js-roadtrip-2/lvl-4/04_refactoring-for-efficiency-and-legibility.js
@@ -5,11 +5,8 @@
 // line that returns a value.
 
 
-///////////////////////// PROBLEM /////////////////////////
+///////////////////////// SOLUTION /////////////////////////
 
 function mystery(x, y) {
-  var a = 4 * x * y;
-  var b = 3 * y + 5;
-  var c = a + b;
-  return c;
+  return (4 * x * y) + (3 * y + 5);
 }
