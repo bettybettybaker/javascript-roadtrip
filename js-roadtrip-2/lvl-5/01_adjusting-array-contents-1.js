17568377fffa0c7bd0036301d6fa6df6fe0762db
Betty Baker <betty@heysparkbox.com>
2017-03-20 14:43:06 -0400

    Adjusting Array Contents 1 < ex1 < Level 5

---
+++ js-roadtrip-2/lvl-5/01_adjusting-array-contents-1.js
@@ -0,0 +1,13 @@
+// Challenge:
+
+// In the following array, set one value of the array so that
+// it will be a list of numbers in order.
+
+// var list = [1, 2, 3, 7, 5, 6, 7, 8, 9];
+
+///////////////////////// PROBLEM /////////////////////////
+
+> var list = [1, 2, 3, 7, 5, 6, 7, 8, 9];
+undefined
+
+>
