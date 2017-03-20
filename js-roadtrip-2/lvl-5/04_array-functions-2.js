59415ee912f24f96e269fc3839417ef98247f322
Betty Baker <betty@heysparkbox.com>
2017-03-20 14:53:56 -0400

    Array Functions 2 < ex4 < Level 5

---
+++ js-roadtrip-2/lvl-5/04_array-functions-2.js
@@ -0,0 +1,13 @@
+// Challenge:
+
+// Now using the specific array function that takes a piece of
+// data off the back of an array, remove the last entry from
+// your values array and store the result in a variable
+// called bool.
+
+///////////////////////// PROBLEM /////////////////////////
+
+> var values = ["Jason", 4, true];
+undefined
+
+>
