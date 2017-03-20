7bea450d68152615099433f5e9fda2a7449c7d77
Betty Baker <betty@heysparkbox.com>
2017-03-20 10:40:44 -0400

    Some Dam Complex Conditionals < ex5 < Level 2 [SOLVED]

--- js-roadtrip-2/lvl-2/05_some-dam-complex-conditionals.js
+++ js-roadtrip-2/lvl-2/05_some-dam-complex-conditionals.js
@@ -25,7 +25,19 @@
 
 
 
-///////////////////////// PROBLEM /////////////////////////
+///////////////////////// SOLUTION /////////////////////////
 
 var totalGen = 19;
 var totalMW = 0;
+
+for (currentGen = 1; currentGen <= totalGen; currentGen++) {
+  if (currentGen % 2 != 0) {
+  	console.log("Generator #" + currentGen + " is off.");
+  } else if (currentGen <= 4) {
+  	totalMW = totalMW + 62;
+    console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
+  } else {
+    totalMW = totalMW + 124;
+    console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
+  }
+}
