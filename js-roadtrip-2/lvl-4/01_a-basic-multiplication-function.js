1fa7432a00ccd69a24f104790fe09a48be841d74
Betty Baker <betty@heysparkbox.com>
2017-03-20 11:57:38 -0400

    A Basic Multiplication Function < ex1 < Level 4

---
+++ js-roadtrip-2/lvl-4/01_a-basic-multiplication-function.js
@@ -0,0 +1,9 @@
+// Challenge:
+
+// In the uniqueMath.js file, build a function declaration called
+// multiplyTrio that takes in three parameters. Inside the function,
+// multiply the numbers together into one product, and return the result.
+// You may use whatever parameter and variable names you’d like.
+
+
+///////////////////////// PROBLEM /////////////////////////
