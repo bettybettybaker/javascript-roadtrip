04e0a2a07ca715c891d0fd8fc4dd937b414ab68f
Betty Baker <betty@heysparkbox.com>
2017-03-20 13:13:40 -0400

    Refactoring for Efficiency and Legibility < ex4 < Level 4

---
+++ js-roadtrip-2/lvl-4/04_refactoring-for-efficiency-and-legibility.js
@@ -0,0 +1,15 @@
+// Challenge:
+
+// In uniqueMath.js, you’ll see a function called mystery.
+// Refactor the code in the function to include only one
+// line that returns a value.
+
+
+///////////////////////// PROBLEM /////////////////////////
+
+function mystery(x, y) {
+  var a = 4 * x * y;
+  var b = 3 * y + 5;
+  var c = a + b;
+  return c;
+}
